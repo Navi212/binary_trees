90bd98f0f016e3f047b960a0ca5716c392917dd8
Nweke Joseph <josephchristo615@gmail.com>
2023-06-01 10:44:26 +0100

    Adds 10 - 11 file

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -10,9 +10,9 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 {
 	size_t depth = 0;
 
-	if (tree->left && tree->right)
+	if (!tree)
 		return (0);
-
-	depth = binary_tree_depth(tree->parent);
-	return (depth + 1);
+	for (depth = 0; tree->parent; depth++)
+		tree = tree->parent;
+	return (depth);
 }
