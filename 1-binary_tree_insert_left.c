881a7c804024279bc1d0e94b9890f00d46976f15
Nweke Joseph <josephchristo615@gmail.com>
2023-06-03 13:50:36 +0100

    Updates task 1 file

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -20,7 +20,10 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		return (NULL);
 
 	if (!parent->left)
+	{
 		parent->left = newnode;
+		newnode->parent = parent;
+	}
 	else
 	{
 		newnode->left = parent->left;
