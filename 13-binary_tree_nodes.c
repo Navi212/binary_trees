5f1234709a895796a4cfa6d695c75c7e7727d187
Joseph Nweke <114510416+Navi212@users.noreply.github.com>
2023-06-04 21:49:03 +0100

    Update 13-binary_tree_nodes.c

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -10,10 +10,8 @@
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-	size_t left = 0;
-	size_t right = 0;
-	size_t count = 0;
-
+	size_t left, right, count = 0;
+	
 	if (!tree)
 		return (0);
 	if (tree->left || tree->right)
